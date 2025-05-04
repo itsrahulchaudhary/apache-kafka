ea3bd9491c50ee16a448126a6fab61bd2f28383a
itsrahulchaudhary <rahulcy49@gmail.com>
2025-05-04 16:37:40 +0530

    add...

--- 03-spring-boot-apache-kafka/src/main/java/com/rahulit/ApacheKafkaPocApplication.java
+++ 03-spring-boot-apache-kafka/src/main/java/com/rahulit/ApacheKafkaPocApplication.java
@@ -3,11 +3,11 @@ package com.rahulit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//
 @SpringBootApplication
 public class ApacheKafkaPocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApacheKafkaPocApplication.class, args);//
+		SpringApplication.run(ApacheKafkaPocApplication.class, args);
 	}
 }
